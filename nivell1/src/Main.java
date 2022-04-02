c69c6e48dfd0fd3e8a20fbefe8e2ba150410aaff
4SH1N <20546366+haustasis@users.noreply.github.com>
2022-04-02 05:30:46 +0200

    Enunciats

---
+++ nivell1/src/Main.java
@@ -0,0 +1,13 @@
+
+/*
+Singleton
+Crea una classe que repliqui el funcionament del comando 'Undo'. Aquesta classe serà utilitzada per la classe Main,
+ que permetrà introduir opcions per consola.
+
+La classe 'Undo' ha de guardar memòria dels últims comandos introduïts, permetent afegir o eliminar comandos,
+així com llistar els ultimos comandos introduïts (similar al comando 'history' en Linux).
+La classe 'Undo' ha d'implementar imprescindiblement un patró Singleton.
+ */
+
+public class Main {
+}
