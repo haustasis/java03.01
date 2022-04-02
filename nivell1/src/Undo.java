0ae476d5278468c9a089e9144bdd25d8f8969074
4SH1N <20546366+haustasis@users.noreply.github.com>
2022-04-02 08:04:45 +0200

    Main Undo singleton

--- nivell1/src/Undo.java
+++ nivell1/src/Undo.java
@@ -1,2 +1,33 @@
+import java.util.ArrayList;
+
 public class Undo {
+
+    private static Undo instancia;
+    private static ArrayList<String> comandes = new ArrayList<>();
+
+    private Undo() {}
+
+    public static Undo getInstancia() {
+        if (instancia == null) {
+            instancia = new Undo();
+        }
+        return instancia;
+    }
+
+    public void afegirComanda(String comanda) {
+        comandes.add(comanda);
+    }
+
+    public void eliminarComanda() {
+
+        if (comandes.isEmpty() == false) {
+            comandes.remove(comandes.size() - 1);
+        }
+
+    }
+
+    public void historialComandes() {
+        System.out.println(comandes);
+    }
+
 }
